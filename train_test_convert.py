67dcf91ba95a8083cffb22540bc27b660e6e6891
Thevaporiser <67265952+Thevaporiser@users.noreply.github.com>
2020-10-16 14:53:04 +0530

    Update train_test_convert.py

--- train_test_convert.py
+++ train_test_convert.py
@@ -1,14 +1,16 @@
+'''
 import os
 os.chdir("/Users/priyanshringe/Documents/College_Stuff/Codes-and-stuff/minorProject")
+'''
 
 import numpy as np
 import pandas as pd
 from wavToSpectrogram_2 import wave2Spec as w2s
 
-path = '/Users/priyanshringe/Documents/College_Stuff/DATASETS/minor_dataset/data/'
-saveImg = '/Users/priyanshringe/Documents/College_stuff/DATASETS/minor_dataset/data/TEST_IMAGES' #change TRAIN_IMAGES to TEST_IMAGES
+path = 'Path/to/folder/data/'
+saveImg = '/Path/to/folder/data/TEST' #change TRAIN to TEST
 #in below line, change train_data to test_data and select the path column name for windows and change nrows value ( for TRAIN = 23100, for TEST = 8400)
-Data = pd.read_csv("/Users/priyanshringe/Documents/College_Stuff/DATASETS/minor_dataset/test_data.csv", dtype = {"dialect_region": str, "speaker_id": str, "path_from_data_dir": str,"filename": str, "is_audio": bool}, nrows=8400, usecols =['dialect_region','speaker_id','filename','path_from_data_dir','is_audio'])
+Data = pd.read_csv("/Path/to/csv/file/test_data.csv", dtype = {"dialect_region": str, "speaker_id": str, "path_from_data_dir": str,"filename": str, "is_audio": bool}, nrows=8400, usecols =['dialect_region','speaker_id','filename','path_from_data_dir','is_audio'])
 audioBool = np.array(Data["is_audio"])
 #change to windows file path column as stated in the csv file in the below line 
 audio_path = np.array(Data["path_from_data_dir"])
@@ -35,7 +37,7 @@ appPath = np.array(appPath)
 imgfileArray = np.array(imgFile)
 
 for i in range(len(imgfileArray)):
-    im.append(imgfileArray[i][:-4] + '.png')
+    im.append(audio_path[i][6:-4] + '.png')
 
 imArray = np.array(im)
 
@@ -47,4 +49,4 @@ print(len(appPath))
 
 for lndex in range(len(appPath)):
     w2s.convert(appPath[lndex], imgSave[lndex])
-    print(lndex)
+    print(lndex + " files converted out of " + len(appPath))
