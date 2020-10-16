33a8b81ff5ce3b8927f2fff3fd2ca66bb476c342
Thevaporiser <67265952+Thevaporiser@users.noreply.github.com>
2020-10-16 14:55:14 +0530

    Update wavToSpectrogram_2.py

--- wavToSpectrogram_2.py
+++ wavToSpectrogram_2.py
@@ -12,14 +12,9 @@ import IPython.display as ipd
 class wave2Spec:
     
     def convert(audio_clips, save_image):
-        #audio_fpath = "/Users/priyanshringe/Documents/College_Stuff/DATASETS/minor_dataset/testSpectro"
-        #audio_clips = os.listdir(audio_fpath)
-        #print("No. of .wav files in audio folder = ",len(audio_clips))
-
+        
         x, sr = librosa.load(audio_clips, sr=22050)#44100)
-
-        #print(type(x), type(sr))
-        #print(x.shape, sr)
+        
         '''
         plt.figure(figsize=(14, 5))
         librosa.display.waveplot(x, sr=sr)
@@ -30,6 +25,7 @@ class wave2Spec:
         librosa.display.specshow(Xdb, sr=sr, x_axis='time', y_axis='hz')
         plt.colorbar()
         '''
+        
         X = librosa.stft(x)
         Xdb = librosa.amplitude_to_db(abs(X))
         
